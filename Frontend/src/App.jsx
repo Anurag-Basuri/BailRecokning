025b9cc07f619ccd96e5594cf96f6a5d6e1ada21
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2024-09-04 02:17:54 +0530

    enviroment setup
    download packages
    like express nodemon mongoose etc
    for frontend using vite and tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='border p-10 text-center bg-slate-500'> Bail Reckoner</h1>
+        <h1> work in progress </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
